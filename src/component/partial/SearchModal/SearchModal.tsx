14c011f2324c95126e6eb1c232a65ae13e1cf305
farmerpaul <paul.hh@metalab.co>
2022-05-19 12:00:14 -0400

    Search modal functionality done

--- src/component/partial/SearchModal/SearchModal.tsx
+++ src/component/partial/SearchModal/SearchModal.tsx
@@ -1,36 +1,110 @@
-import React, {useRef, useState} from 'react';
-import {StyleSheet, TextInput} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, StyleSheet, TextInput, View} from 'react-native';
 
-import {Modal} from '/component/base';
+import {Icon, Modal, Text} from '/component/base';
 import {ModalProps} from '/component/base/Modal/Modal';
 import {SearchInput} from '/component/partial';
+import searchMockData from '/fixtures/search';
+import {colors, spacing} from '/theme';
 
 export type Props = ModalProps & {
-  onSelect?: (result: string) => void;
+  onSelect?: (value: string) => void;
 };
 
 const styles = StyleSheet.create({
-  input: {
+  searchInput: {
     marginLeft: 72,
+  },
+  container: {
+    marginHorizontal: spacing.gutter,
+  },
+  title: {
+    marginTop: 24,
+    marginBottom: 16,
+  },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  optionIcon: {
+    marginRight: 8,
   },
 });
 
-const SearchModal: React.FC<Props> = ({onSelect: _onSelect, ...rest}) => {
-  const ref = useRef<TextInput>(null);
-  const [value, setValue] = useState('');
+const SearchModal: React.FC<Props> = ({onSelect, setVisible, ...rest}) => {
+  const inputRef = useRef<TextInput>(null);
+  const [searchText, setSearchText] = useState('');
+  const options = searchMockData.cities;
+  const [filteredOptions, setFilteredOptions] = useState<string[]>(options);
+
+  /* Effects
+  =================================================== */
+  // Filter list when search text changes.
+  useEffect(() => {
+    if (searchText) {
+      setFilteredOptions(
+        searchText
+          ? options.filter(option =>
+              option.toUpperCase().includes(searchText.toUpperCase()),
+            )
+          : options,
+      );
+    } else {
+      // Else set it to all options.
+      setFilteredOptions(options);
+    }
+  }, [searchText, options]);
+
+  /* Event handlers.
+  =================================================== */
+  // Focus search input when modal is displayed.
+  const onModalShow = () => {
+    inputRef.current?.focus();
+  };
+
+  // Clear search text when modal is closed.
+  const onModalDismiss = () => {
+    setSearchText('');
+  };
+
+  // Trigger selection event handler and close modal.
+  const onSelectItem = (value: string) => {
+    onSelect?.(value);
+    setVisible(false);
+  };
 
   return (
     <Modal
       accessibilityLabel="Search for a city"
-      animationType="fade"
-      onShow={() => ref.current?.focus()}
+      onShow={onModalShow}
+      onDismiss={onModalDismiss}
+      setVisible={setVisible}
       {...rest}>
       <SearchInput
-        ref={ref}
-        value={value}
-        onChangeText={setValue}
-        style={styles.input}
+        ref={inputRef}
+        value={searchText}
+        onChangeText={setSearchText}
+        style={styles.searchInput}
       />
+      <View style={styles.container}>
+        <Text variant="title" style={styles.title}>
+          Getaways Near You
+        </Text>
+        {filteredOptions.map(option => (
+          <Pressable
+            key={option}
+            onPress={() => onSelectItem(option)}
+            style={styles.option}>
+            <Icon
+              svg={require('/asset/svg/map-marker.svg')}
+              color={colors.blue}
+              style={styles.optionIcon}
+            />
+            <Text variant="base">{option}</Text>
+          </Pressable>
+        ))}
+      </View>
     </Modal>
   );
 };
